ba6847fd1db9c97e1c87de80af8b48cfc224ec63
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-19 19:33:59 -0400

    add dynamic button classnames and text

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignup.scss'
 
 import user_icon from '../../assets/person.png'
@@ -6,10 +6,13 @@ import email_icon from '../../assets/email.png'
 import password_icon from '../../assets/password.png'
 
 const LoginSignup = () => {
+
+    const [action, setAction] = useState("Sign Up")
+
   return (
     <div className="container">
         <div className="header">
-            <div className="text">Sign Up</div>
+            <div className="text">{action}</div>
             <div className="underline"></div>
         </div>
         <div className="inputs">
@@ -31,8 +34,8 @@ const LoginSignup = () => {
         </div>
         <div className="forgot-password">Lost Password? <span>Click Here</span></div>
         <div className="submit-container">
-            <div className="submit">Sign Up</div>
-            <div className="submit">Login</div>
+            <div className={action==="Login"?"submit gray" : "submit"}>Sign Up</div>
+            <div className={action==="Sign Up"?"submit gray" : "submit"}>Login</div>
         </div>
     </div>
   )
