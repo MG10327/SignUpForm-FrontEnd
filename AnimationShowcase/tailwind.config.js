1e93c65b22ff7ac21dc6f5f9491bd5b4148601da
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-23 17:45:53 -0500

    update styles

--- AnimationShowcase/tailwind.config.js
+++ AnimationShowcase/tailwind.config.js
@@ -12,6 +12,22 @@ export default {
         'circular-web': ['circular-web', 'san-serif'],
         'robert-medium': ['robert-medium', 'san-serif'],
         'robert-regular': ['robert-regular', 'san-serif'],
+      },
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#DFDFF2",
+          100: "#FOF2FA",
+          200: "#010101",
+          300: "#4fb7dd",
+        },
+        violet: {
+          300: '#5724FF',
+        },
+        yellow: {
+          100: "#8E983F",
+          300: "#EDFF66"
+        }
       }
     },
   },
