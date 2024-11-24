319d6172574467014c13cdc03f6b48918983bf75
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-23 23:34:29 -0500

    start polygon animations

--- AnimationShowcase/src/components/Hero.jsx
+++ AnimationShowcase/src/components/Hero.jsx
@@ -38,6 +38,25 @@ const Hero = () => {
         }
     );
 
+    useGSAP(() => {
+        gsap.set('#video-frame', {
+            clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+            borderRadius: '0 0 40% 10%'
+        })
+
+        gsap.from('#video-frame', {
+            clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+            borderRadius: '0 0 0 0',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '#video-frame',
+                start: 'center center',
+                end: 'bottom center',
+                scrub: true
+            }
+        })
+    })
+
     const totalVideos = 4
 
     const nextVideoRef = useRef(null)
