4909c944921d9be5ea1cff2b8cc49fd8ac00ab13
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-23 23:42:56 -0500

    scroll animation update

--- AnimationShowcase/src/components/Hero.jsx
+++ AnimationShowcase/src/components/Hero.jsx
@@ -1,8 +1,11 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Button from './Button'
 import { TiLocationArrow } from "react-icons/ti";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger) // This enables scroll trigger as an animation 
 
 const Hero = () => {
 
@@ -10,6 +13,12 @@ const Hero = () => {
     const [hasClicked, setHasClicked] = useState(false)
     const [isLoading, setIsLoading] = useState(true)
     const [loadedVideos, setLoadedVideos] = useState(0);
+
+    useEffect(() => {
+        if (loadedVideos === totalVideos - 1) {
+            setIsLoading(false);
+        }
+      }, [loadedVideos]);
 
     useGSAP(
         () => {
@@ -77,7 +86,17 @@ const Hero = () => {
 
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
- 
+
+        {isLoading && (
+            <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50'>
+                <div className="three-body">
+                    <div className='three-body__dot' />
+                    <div className='three-body__dot' />
+                    <div className='three-body__dot' />
+                </div>
+            </div>
+        )}
+
         <div
             id="video-frame"
             className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
